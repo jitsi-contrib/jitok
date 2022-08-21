060916ba47a77a5fccb495f0fed0f9e5dcf7b09e
emrah <emrah.com@gmail.com>
2022-08-21 21:36:14 +0300

    refactor: set localhost for API

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://jitok.emrah.com/api";
-//export const API_URL = "http://127.0.0.1:9000/api";
+//export const API_URL = "https://jitok.emrah.com/api";
+export const API_URL = "http://127.0.0.1:9000/api";
