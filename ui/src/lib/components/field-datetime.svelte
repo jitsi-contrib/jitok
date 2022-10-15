94ce84bc641f0d828bf784f290de76bf76278ecb
emrah <emrah.eryilmaz@nordeck.net>
2022-10-15 13:56:33 +0300

    feat: add form-datetime component

---
+++ ui/src/lib/components/field-datetime.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let name: string;
+  export let value: string;
+  export let required = false;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-floating my-1">
+  <input
+    type="datetime-local"
+    class="form-control"
+    id={name}
+    bind:value
+    {required}
+  />
+  <label for={name}>{name}</label>
+</div>
