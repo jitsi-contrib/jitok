d5a63403035b09db2bb94cb9821ce1e8b130349b
emrah <emrah.eryilmaz@nordeck.net>
2023-11-09 12:31:13 +0300

    ui, feat: add radio-inline component

---
+++ ui/src/lib/components/field-radio-inline.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Option } from "$lib/custom-types";
+
+  export let title: string;
+  export let name: string;
+  export let value: undefined | number | string;
+  export let options: Option[];
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<h6 class="text-muted mt-4">{title}</h6>
+
+{#each options as { optName, optValue }}
+  <div class="form-check form-check-inline">
+    <input
+      class="form-check-input"
+      type="radio"
+      {name}
+      bind:group={value}
+      id="{name} {optName}"
+      value={optValue}
+    />
+    <label class="form-check-label" for="{name} {optName}">
+      {optName}
+    </label>
+  </div>
+{/each}
