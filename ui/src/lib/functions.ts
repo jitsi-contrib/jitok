d7d5dcf33cc3e9ef678c73d5eb72222bc1e8d52a
emrah <emrah.eryilmaz@nordeck.net>
2023-11-09 13:51:30 +0300

    ui, refactor: check undefined room password

--- ui/src/lib/functions.ts
+++ ui/src/lib/functions.ts
@@ -1,9 +1,17 @@
 import type { Payload } from "$lib/custom-types";
 
 export async function getToken(apiUrl: string, p: Payload) {
+  const _p = JSON.parse(JSON.stringify(p));
+
+  // undefined as value is used to differ the empty password and an undefined
+  // one
+  if (_p.cntx_room_password === "undefined") {
+    _p.cntx_room_password = undefined;
+  }
+
   const req = new Request(apiUrl, {
     method: "POST",
-    body: JSON.stringify(p),
+    body: JSON.stringify(_p),
     mode: "cors",
   });
 
